96a8096793a10fdf0d8c744bd64001995bd5ffe3
junhyoung <zz7193@naver.com>
2017-10-30 18:53:55 +0900

    db schema modify

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117021736) do
+ActiveRecord::Schema.define(version: 20171025074412) do
+
+  create_table "applicant_result_at_chllenges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "challenges_id"
+    t.integer  "applicants_id"
+    t.integer  "score"
+    t.string   "attachment"
+    t.text     "log",           limit: 65535
+    t.string   "language"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["applicants_id"], name: "index_applicant_result_at_chllenges_on_applicants_id", using: :btree
+    t.index ["challenges_id"], name: "index_applicant_result_at_chllenges_on_challenges_id", using: :btree
+  end
 
   create_table "applicants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "email"
     t.string   "token"
-    t.integer  "score"
-    t.string   "language",                   default: "SpringBoot"
-    t.integer  "challenge_id"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.string   "attachment"
-    t.string   "attachments3"
-    t.text     "log",          limit: 65535
-    t.index ["challenge_id"], name: "index_applicants_on_challenge_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "challenges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
